e21c54493cbbcbcae66b03f42b91b8888aae2b0f
Jakhongir Islomjonov <jahongirnamti@gmail.com>
2024-12-26 13:55:18 +0500

    add  credit method

--- src/main/java/uz/dev/cardprocess/service/TransactionService.java
+++ src/main/java/uz/dev/cardprocess/service/TransactionService.java
@@ -1,6 +1,7 @@
 package uz.dev.cardprocess.service;
 
 import org.springframework.stereotype.Service;
+import uz.dev.cardprocess.dto.CreditRequestDTO;
 import uz.dev.cardprocess.dto.DebitRequestDTO;
 import uz.dev.cardprocess.entity.Card;
 import uz.dev.cardprocess.entity.Transaction;
@@ -8,4 +9,5 @@ import uz.dev.cardprocess.entity.Transaction;
 @Service
 public interface TransactionService {
         Transaction saveDebitTransaction(DebitRequestDTO debitDTO, long newBalance, Card card, Long exchangeRate);
+        Transaction saveDebitTransaction(CreditRequestDTO creditRequestDTO, long newBalance, Card card, Long exchangeRate);
 }
